a1e9e97a7d4e423beec05534097e126d4979e7ec
Jan Lewandowski <iasiu@icloud.com>
2020-04-07 21:04:08 +0200

    finished ui and connected images and button to ViewController.swift

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // refrence to the button which rolls the dices
+    @IBOutlet weak var rollButton: UIButton!
+    // image displaying dice on the left
+    @IBOutlet weak var firstDiceImageView: UIImageView!
+    // image displaying dice on the right
+    @IBOutlet weak var secondDiceImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // give the rollButton rounded corners
+        rollButton.layer.cornerRadius = 5
+        
+        // set the image of firstDice to DiceSix
+        firstDiceImageView.image = #imageLiteral(resourceName: "DiceSix")
     }
 
 
