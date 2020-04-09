9d110d9fe8ead5c35a1d62b6fffa6c7cbff398b1
Jan Lewandowski <iasiu@icloud.com>
2020-04-09 09:38:07 +0200

    refactored code

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -16,13 +16,13 @@ class ViewController: UIViewController {
     // refrence to image displaying dice on the right
     @IBOutlet weak var dice2ImageView: UIImageView!
     
-    // dict with values and corresponding dice image names
-    var imageNames = [1: "DiceOne",
-                      2: "DiceTwo",
-                      3: "DiceThree",
-                      4: "DiceFour",
-                      5: "DiceFive",
-                      6: "DiceSix"]
+    // array with dice image names
+    let imageNames = ["DiceOne",
+                      "DiceTwo",
+                      "DiceThree",
+                      "DiceFour",
+                      "DiceFive",
+                      "DiceSix"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,16 +30,13 @@ class ViewController: UIViewController {
         // give the rollButton rounded corners
         rollButton.layer.cornerRadius = 5
         
-        // set the images of dice1 and dice2 to DiceOne
-        dice1ImageView.image = UIImage(named: "DiceOne")
-        dice2ImageView.image = UIImage(named: "DiceOne")
     }
     
     // triggered when rollButton is pressed
     @IBAction func rollButtonTouchUp(_ sender: UIButton) {
-        // change dice images to new random
-        dice1ImageView.image = UIImage(named: imageNames[Int.random(in: 1 ... 6)]!)
-        dice2ImageView.image = UIImage(named: imageNames[Int.random(in: 1 ... 6)]!)
+        // change dice images to new random image
+        dice1ImageView.image = UIImage(named: imageNames.randomElement()!)
+        dice2ImageView.image = UIImage(named: imageNames.randomElement()!)
     }
 }
 
